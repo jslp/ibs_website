cbd347df5f3ead82797ed9ae7c00067d86a3243e
Jonas Schlapp <jonas.schlapp@mni.thm.de>
2016-01-18 16:54:15 +0100

    XMLHTTPRequest erstellt und geöffnet

--- js/webapp.js
+++ js/webapp.js
@@ -1,2 +1,11 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "")
+var xhr;
+
+if (window.XMLHttpRequest) {
+    xhr = new XMLHttpRequest();
+    } else {
+    // code for IE6, IE5
+    xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+}
+
+xhr.open("GET","https://arsnova.eu/api/statistics", false);
+
